41827930a54b4bef338860e99ce36af476782959
Santiago Rojas <sajhu93@gmail.com>
2013-06-10 23:05:30 -0500

    Change default index template
    also added some info about the project

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>FurrySkeleton | Bootstrap</title>
+    <title>FurrySkeleton</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <!-- Le styles -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
+    <style type="text/css">
       body {
-        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+        padding-top: 60px;
+        padding-bottom: 40px;
       }
     </style>
-    <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -24,7 +23,11 @@
     <![endif]-->
 
     <!-- Fav and touch icons -->
-   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
   </head>
 
   <body>
@@ -37,255 +40,69 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="brand" href="#">Project name</a>
+          <a class="brand" href="#">Furry Skeleton</a>
           <div class="nav-collapse collapse">
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#contact">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
             </ul>
+            <form class="navbar-form pull-right">
+              <input class="span2" type="text" placeholder="Email">
+              <input class="span2" type="password" placeholder="Password">
+              <button type="submit" class="btn">Sign in</button>
+            </form>
           </div><!--/.nav-collapse -->
         </div>
       </div>
     </div>
 
     <div class="container">
-<!-- HEADER END -->
 
-<!-- Header -->
-<div id="top-nav" class="navbar navbar-inverse navbar-static-top">
-  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-    <span class="icon-list"></span>
-  </button>
-  <a class="navbar-brand" href="http://www.bootply.com">Dashboard</a>
-  <div class="nav-collapse collapse navbar-responsive-collapse">
-    <ul class="nav navbar-nav pull-right">
-      
-      <li class="dropdown">
-        <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#">
-          <strong>Admin</strong> <span class="caret"></span></a>
-        <ul id="g-account-menu" class="dropdown-menu" role="menu">
-          <li><a href="#">My Profile</a></li>
-        </ul>
-      </li>
-      
-      <li><a href="#">Log out</a></li>
-    </ul>
-  </div>
-</div>
-<!-- /Header -->
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>Hello, Developer!</h1>
+        <p>This is a framework for starting a quick Web App PHP+MySQL development. It is based on the wonderfulness of Twitter's <a href="http://twitter.github.com/bootstrap">Bootstrap</a> extended by <a href="http://jasny.github.io/bootstrap">Jasny</a> and other open source libraries.</p>
+        <p><a href="https://github.com/sajhu/FurrySkeleton" class="btn btn-primary btn-large">Watch this at Github &raquo;</a><a href="http://development.santiagorojas.co/FurrySkeleton" class="btn btn-primary btn-large pull-right">See Docs & More &raquo;</a></p>
+      </div>
 
-<!-- Main -->
-
-<div class="row">
-      <div class="col col-lg-3">
-      <!-- Left -->
-      <ul class="nav nav-pills nav-stacked">
-        <li class="nav-header"></li>
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="http://www.bootply.com">Bootply</a></li>
-        <li class="divider"></li>
-        <li><a href="#">Link 1</a></li>
-        <li><a href="#">Link 2</a></li>
-        <li><a href="#">Link ..</a></li>
-        <li><a href="#">Link ..</a></li>
-        <li class="divider"></li>
-        <li><a href="#">Link ..</a></li>
-        <li><a href="#">Link ..</a></li>
-        <li><a href="#">Link ..</a></li>
-        <li><a href="#">Link ..</a></li>
-        <li class="divider"></li>
-        <li><a href="#">Link ..</a></li>
-        <li><a href="#">Link ..</a></li>
-        <li><a href="#">Link ..</a></li>
-        <li><a href="#">Link ..</a></li>
-      </ul>
-  </div><!-- /span-3 -->
-  
-    <div class="col col-lg-9">
-        <!-- Right -->
-      <div class="navbar-inner clearfix">
-            <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-dashboard icon-large"></span> My Dashboard</a>
-            <ul class="nav nav-pills pull-right">
-              <li class="active"><a title="Add Widget" data-toggle="modal" href="#addWidgetModal"><span class="glyphicon glyphicon-plus-sign icon-large"></span> Add Widget</a></li>
-            </ul>
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
         </div>
-      
-        
-      
-    <div class="row">
-          <div class="col col-lg-6">
-        <div class="well">Inbox Messages <span class="badge pull-right">3</span></div>
-              
-                <div class="panel">
-                    <div class="panel-heading">
-                      <i class="icon icon-chevron-up chevron"></i>
-                      <i class="icon icon-wrench pull-right"></i> Quick Start
-                  </div>
-                  <div class="panel-content">
-                    
-                      <div class="btn-group btn-group-justified">
-                        <a href="#" class="btn btn-primary col col-lg-3">
-                          <i class="icon-laptop icon-large"></i>
-                          <p>Service</p>
-                        </a>
-                        <a href="#" class="btn btn-primary col col-lg-3">
-                          <i class="icon-cloud icon-large"></i>
-                          <p>Cloud</p>
-                        </a>
-                        <a href="#" class="btn btn-primary col col-lg-3">
-                          <i class="icon-github icon-large"></i>
-                          <p>GitHub</p>
-                        </a>
-                        <a href="#" class="btn btn-primary col col-lg-3">
-                          <span class="glyphicon glyphicon-question-sign icon-large"></span>
-                          <p>Help</p>
-                        </a>
-                      </div>
-                  </div><!--/panel content-->
-              </div><!--/panel-->
-              
-              
-              <div class="panel">
-                  <div class="panel-heading">Report</div>
-                  <table class="table table-striped">
-                  <thead>
-                    <tr><th>Col 1</th><th>Col 2</th><th>Col 3</th></tr></thead>
-                  <tbody>
-                    <tr><td>45</td><td>2.45%</td><td>Direct</td></tr>
-                    <tr><td>289</td><td>56.2%</td><td>Referral</td></tr>
-                    <tr><td>98</td><td>25%</td><td>Type</td></tr>
-                    <tr><td>..</td><td>..</td><td>..</td></tr>
-                    <tr><td>..</td><td>..</td><td>..</td></tr>
-                  </tbody>
-                  </table>
-              </div><!--/panel-->
-              
-              
-              <div class="panel">
-                  <div class="panel-heading">Charts</div>
-                <div class="panel-content"><img src="http://www.wired.com/playbook/wp-content/uploads/2012/08/data-tracking-run-chart.jpg"></div>
-              </div><!--/panel-->
-              
-            </div>
-          <div class="col col-lg-6">
-        <div class="panel">
-                  <div class="panel-heading">Bootstrap 3</div>
-                  This is a dashboard-style layout that uses Bootstrap 3 RC. You can follow the ongoing pre-release discussions about Bootstrap 3.0 on this <a href="https://github.com/twitter/bootstrap/pull/6342">GitHub thread</a>.
-                </div>
-              
-                <div class="panel">
-                  <div class="panel-heading"><i class="icon icon-chevron-up chevron"></i>
-                      <i class="icon icon-wrench pull-right"></i> Form
-                  </div>
-                  <div class="panel-content form-horizontaL">
-
-                      <form class="form form-vertical">
-                       
-                        <div class="control-group">
-                          <label>Name</label>
-                          <div class="controls">
-                           <input type="text" placeholder="Enter Name">
-                           
-                          </div>
-                        </div>      
-                        
-                        <div class="control-group">
-                          <label>Label</label>
-                          <div class="controls">
-                           <input type="password" placeholder="Password">
-                          
-                          </div>
-                        </div>   
-                        
-                        <div class="control-group">
-                          <label>Text</label>
-                          <div class="controls">
-                            <textarea></textarea>
-                          </div>
-                        </div> 
-                             
-                        <div class="control-group">
-                          <label>Select</label>
-                          <div class="controls">
-                             <select><option>options</option></select>
-                          </div>
-                        </div>    
-                        
-                        <div class="control-group">
-                            <label></label>
-                          <div class="controls">
-                          <button type="submit" class="btn btn-primary">
-                              Post
-                            </button>
-                          </div>
-                        </div>   
-                        
-                      </form>
-                
-                
-                  </div><!--/panel content-->
-                </div><!--/panel-->
-              
-                <div class="panel">
-                  <div class="panel-heading">Engagement</div>
-                  <div class="panel-content text-centered"> 
-                    
-                    <img src="http://placehold.it/90X90/CC2222/FFF" class="img-circle">
-                    
-                    <img src="http://placehold.it/90X90/11CC22/FFF" class="img-circle">
-                    
-                    <img src="http://placehold.it/90X90/EEEEEE/222" class="img-circle">
-                    
-                    <img src="http://placehold.it/90X90/d9d9d9/FFF" class="img-circle">
-                    
-                  </div>
-               </div><!--/panel-->
-              
-              
-                <i class="icon-bar-chart icon-3x"></i>
-                <i class="icon-plus icon-3x"></i>
-                <i class="icon-facebook icon-3x"></i>
-                <i class="icon-beaker icon-3x"></i>
-                <i class="icon-twitter icon-3x"></i>
-                
-              
-      </div><!--/col-span-6-->
-     
-      </div><!--/row-->
-    </div><!--/col-span-9--> 
-</div>
-
-<div class="container">
-  <hr>
-  <a href="http://www.bootply.com/61582" title="Bootstrap 3 Dashboard">Edit on Bootply</a>
-  <hr>
-</div>
-
-<!-- /Main -->
-
-<div class="modal" id="addWidgetModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h4 class="modal-title">Add Widget</h4>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+       </div>
+        <div class="span4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">View details &raquo;</a></p>
+        </div>
       </div>
-      <div class="modal-body">
-        <p>Add a widget stuff here..</p>
-      </div>
-      <div class="modal-footer">
-        <a href="#" class="btn">Close</a>
-        <a href="#" class="btn btn-primary">Save changes</a>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dalog -->
-</div><!-- /.modal -->
 
+      <hr>
 
-     
+      <footer>
+        <p>&copy; Developed by <a href="http://santiagorojas.co">Santiago Rojas</a>.</p>
+      </footer>
 
-<!-- FOOTER BEGIN -->
     </div> <!-- /container -->
 
     <!-- Le javascript
